41244048e362b4584a7bffbba303d900d554f81b
AshwinLechner <69810256+AshwinLechner@users.noreply.github.com>
2020-10-16 21:08:13 +0200

    Finished project

---
+++ guessing_game.py
@@ -0,0 +1,54 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+"""
+
+import random
+
+
+def start_game():
+
+
+
+# Kick off the program by calling the start_game function.
+start_game()
+
+
+
+def numberGuessed(num):
+    num = int(num) 
+    return num
+
+    
+print("""
+    Welcome to the guessing game!
+    Please guess a number between 1 and 10
+    """)
+
+random_number = random.randint(1, 10)
+
+number_guessed = numberGuessed(input())
+
+number_of_tries = 0
+try:
+    while True:
+        if number_guessed == random_number:
+            print("Thats correct! i took you {} tries to guess the correct number.".format(number_of_tries))
+            break
+        elif number_guessed > random_number:
+            print("the number is lower.  ")
+            number_guessed = numberGuessed(input("please try again "))
+            number_of_tries += 1
+            continue
+        elif number_guessed < random_number:
+            print("the number is higher.  ")
+            number_guessed = numberGuessed(input("please try again "))
+            number_of_tries += 1
+            continue
+    
+except ValueError:
+    print("please select a valid number")
+    
+print("The game is over now, thank you for playing")    
+
